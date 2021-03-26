583b74318972e98949e0684bf47d770ef5a057e6
Maksym-Yurii Rudko <maksym-yurii.rudko@globallogic.com>
2021-03-26 11:51:37 +0200

    Organise code

--- homework_2/ls/src/main.rs
+++ homework_2/ls/src/main.rs
@@ -22,17 +22,9 @@ fn main() -> io::Result<()> {
     if start_path.is_dir() {
         match file_provider::get_files_in_directory(start_path) {
             Ok(mut file_models) => {
-                println!("List of files in {}", start_path.to_string_lossy());
-                println!("{:36} {:9}", "Name", "Size");
-
-                sort_file_table(file_models);
-                for model in file_models {
-                    println!(
-                        "{:36} {:9}",
-                        model.name + if model.is_directory { "/" } else { "" },
-                        model.size.to_human_str()
-                    )
-                }
+                let start_path = start_path.to_string_lossy();
+                sort_file_table(&mut file_models);
+                print_file_table(start_path.as_ref(), &file_models);
             }
             Err(e) => eprintln!("Error reading files in directory: {}", e)
         }
@@ -44,7 +36,20 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn sort_file_table(&mut file_table: Vec<FileModel>) {
+fn print_file_table(start_path: &str, file_table: &Vec<FileModel>) {
+    println!("List of files in {}", start_path);
+    println!("{:36} {:9}", "Name", "Size");
+
+    for model in file_table {
+        println!(
+            "{:36} {:9}",
+            format!("{}{}", model.name, if model.is_directory { "/" } else { "" }),
+            model.size.to_human_str()
+        )
+    }
+}
+
+fn sort_file_table(file_table: &mut Vec<FileModel>) {
     file_table.sort_unstable_by(|a, b| a.is_directory
         .cmp(&b.is_directory)
         .reverse()
