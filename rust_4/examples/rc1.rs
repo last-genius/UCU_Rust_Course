b3e367317a6b440c59c3561a7cc5779a5e341dec
LastGenius-edu <as.andreysultanov@gmail.com>
2021-04-14 15:28:21 +0300

    Update week4 lecture

--- rust_4/examples/rc1.rs
+++ rust_4/examples/rc1.rs
@@ -1,17 +1,21 @@
-let text = "Rc examples".to_string();
-{
-    // Reference count is 1
-    let rc_a: Rc<String> = Rc::new(text);
+use std::rc::Rc;
+
+pub fn main() {
+    let text = "Rc examples".to_string();
     {
-        // Reference count is 2
-        let rc_b: Rc<String> = Rc::clone(&rc_a);
-        
-        rc_a.len();
-        println!("{}", rc_b);
-        
-        // rc_b is dropped, reference count is 1
+        // Reference count is 1
+        let rc_a: Rc<String> = Rc::new(text);
+        {
+            // Reference count is 2
+            let rc_b: Rc<String> = Rc::clone(&rc_a);
+
+            rc_a.len();
+            println!("{}", rc_b);
+
+            // rc_b is dropped, reference count is 1
+        }
+        // rc_a is dropped, reference count is 0, the value is dropped too
     }
-    // rc_a is dropped, reference count is 0, the value is dropped too
+    // Error, rc_examples was dropped!
+    println!("rc_examples: {}", text);
 }
-// Error, rc_examples was dropped!
-println!("rc_examples: {}", rc_examples);
